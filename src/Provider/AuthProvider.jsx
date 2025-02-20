25022b2eb5c7fa0e14ec017bb9f6388bca05a44b
Your Name <ariyaniphone14@gmail.com>
2025-02-20 18:20:46 +0600

    fixed bug in authprovider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,25 +1,70 @@
-import { createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
-import React, { createContext, useState } from "react";
-
-export const authcontext = createContext();
-
-export default function AuthProvider() {
-
-    const [user,setUser]=useState(null);
-    const[loading,setLoading]=useState(true);
-
-    const provider=new GoogleAuthProvider();
-
-
-
-    const createProfile=(email,password)=>{
-        setLoading(true);
-        return createUserWithEmailAndPassword()
-    }
-
-
-
-
-
-  return <div>AuthProvider</div>;
-}
+import {
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+  } from "firebase/auth";
+  import React, { createContext, useEffect, useState } from "react";
+  import { auth } from "../firebase/firebase";
+  
+  export const authcontext = createContext();
+  
+  export default function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    const provider = new GoogleAuthProvider();
+  
+    const createProfile = (email, password) => {
+      setLoading(true);
+      return createUserWithEmailAndPassword(auth, email, password);
+    };
+  
+    const googleLogin = () => {
+      setLoading(true);
+      return signInWithPopup(auth, provider);
+    };
+  
+    const logOut = () => {
+      setLoading(true);
+      return signOut(auth);
+    };
+  
+    const userLogin = (email, password) => {
+      setLoading(true);
+      return signInWithEmailAndPassword(auth, email, password);
+    };
+  
+    const updateUserProfile = (updatedData) => {
+      return updateProfile(auth.currentUser, updatedData);
+    };
+  
+    useEffect(() => {
+      const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+      });
+      return () => unSubscribe();
+    }, []);
+  
+    const authInfo = {
+      user,
+      loading,
+      setLoading,
+      createProfile,
+      userLogin,
+      updateUserProfile,
+      logOut,
+      googleLogin,
+    };
+  
+    return (
+      <authcontext.Provider value={authInfo}>
+        {children}
+      </authcontext.Provider>
+    );
+  }
+  
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Route.jsx'
+import AuthProvider from './Provider/AuthProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <RouterProvider router={router}></RouterProvider>
+    <Toaster />
+    </AuthProvider>
   </StrictMode>,
 )
