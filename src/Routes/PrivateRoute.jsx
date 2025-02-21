fb9c4d0936a3b08d949e6922573fff2ce748df6b
Your Name <ariyaniphone14@gmail.com>
2025-02-21 12:02:30 +0600

    added private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { authcontext } from '../Provider/AuthProvider'
+import { useLocation } from 'react-router-dom';
+
+export default function PrivateRoute({children}) {
+
+    const {user,loading}=useContext(authcontext);
+
+
+    const location=useLocation();
+
+    if (loading) {
+        // return <h1 className='text-blue-600 font-bold text-4xl text-center mt-12'>Loading...</h1>;
+        return <div className='grid justify-items-center mt-12'><span className="loading loading-bars loading-lg text-4xl"></span></div>
+      }
+
+      if(user&& user?.email){
+        return children;
+      }
+
+  return (
+    <Navigate state={location.pathname} to={"/login"} ></Navigate>
+  )
+}
--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,54 +1,60 @@
-import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
-import MainHome from '../Pages/MainHome'
-import Home from '../Components/Home'
-import About from '../Pages/About'
-import Contact from '../Pages/Contact'
-import Login from '../Pages/Login'
-import Register from '../Pages/Register'
-import ErrorPage from '../Pages/ErrorPage'
-import Tasks from '../Pages/Tasks'
-import TaskUpdate from '../Components/subcomponents/TaskUpdate'
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainHome from "../Pages/MainHome";
+import Home from "../Components/Home";
+import About from "../Pages/About";
+import Contact from "../Pages/Contact";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import ErrorPage from "../Pages/ErrorPage";
+import Tasks from "../Pages/Tasks";
+import TaskUpdate from "../Components/subcomponents/TaskUpdate";
+import PrivateRoute from "./PrivateRoute";
 
-const router=createBrowserRouter([
-    {
-        path:"/",
-        element:<MainHome></MainHome>,
-        errorElement:<ErrorPage></ErrorPage>,
-        children:[
-            {
-                path:"/",
-                element:<Home></Home>
-            },
-            {
-                path:"/about",
-                element:<About></About>
-            },
-            {
-                path:"/contact",
-                element:<Contact></Contact>
-            },
-            {
-                path:"/tasks",
-                element:<Tasks></Tasks>
-            },
-            {
-                path:"/tasks/:id",
-                element:<TaskUpdate></TaskUpdate>
-            }
-        ]
-    },
-    {
-        path:"/login",
-        element:<Login></Login>
-    },
-    {
-        path:"/register",
-        element:<Register></Register>
-    }
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainHome></MainHome>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/tasks",
+        element: (
+          <PrivateRoute>
+            <Tasks></Tasks>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/tasks/:id",
+        element: (
+          <PrivateRoute>
+            <TaskUpdate></TaskUpdate>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+]);
 
-
-
-
-export default router
+export default router;
